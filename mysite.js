6e39a6872202139f714e2d63bbf254434795abe1
SnowyJaguar <69837640+SnowyJaguar@users.noreply.github.com>
2021-01-05 02:38:40 -0700

    added menu

--- mysite.js
+++ mysite.js
@@ -8,7 +8,6 @@ function showButtons() {
     this.style.transition = "box-shadow 0.5s ease-out";
 }
 
-
 function unshowButtons() {
     this.style.boxShadow = "0 5px 20px black";
     this.style.transition = "box-shadow 0.5s ease-out";
@@ -18,3 +17,17 @@ for (const project of allProjects) {
     project.addEventListener("mouseover", showButtons);
     project.addEventListener("mouseout", unshowButtons);
 }
+
+const navLinks = document.querySelector("nav ul");
+const burger = document.querySelector(".burger");
+let burgerClicked = false;
+
+burger.onclick = function() {
+    if (!burgerClicked) {
+        navLinks.style.animation = "navLinksFadeIn 0.5s ease-in forwards";
+        burgerClicked = true;
+    } else {
+        navLinks.style.animation = "navLinksFadeOut 0.5s ease-in";
+        burgerClicked = false;
+    }
+};
