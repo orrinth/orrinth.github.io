24213d670c3ec86af0b5cd72aaba9c5cb2681518
SnowyJaguar <69837640+SnowyJaguar@users.noreply.github.com>
2021-01-28 00:02:23 -0700

    new design

--- mysite.js
+++ mysite.js
@@ -1,33 +1,14 @@
-const projects = document.querySelectorAll(".project");
-const siteLinks = document.querySelectorAll(".site-links")
-const allProjects = Array.from(projects);
-const allSiteLinks = Array.from(siteLinks);
+const burger   = document.querySelector('.fa-bars');
+const navLinks = document.querySelector('nav');
+const exitNav  = document.querySelector('.fa-times');
 
-function showButtons() {
-    this.style.boxShadow = "none";
-    this.style.transition = "box-shadow 0.5s ease-out";
+function showmenu() {
+    navLinks.style.transform = 'translate(0%)';
 }
 
-function unshowButtons() {
-    this.style.boxShadow = "0 5px 20px black";
-    this.style.transition = "box-shadow 0.5s ease-out";
+function hidemenu() {
+    navLinks.style.transform = 'translate(100%)';
 }
 
-for (const project of allProjects) {
-    project.addEventListener("mouseover", showButtons);
-    project.addEventListener("mouseout", unshowButtons);
-}
-
-const navLinks = document.querySelector("nav ul");
-const burger = document.querySelector(".burger");
-let burgerClicked = false;
-
-burger.onclick = function() {
-    if (!burgerClicked) {
-        navLinks.style.animation = "navLinksFadeIn 0.5s ease-in forwards";
-        burgerClicked = true;
-    } else {
-        navLinks.style.animation = "navLinksFadeOut 0.5s ease-in";
-        burgerClicked = false;
-    }
-};
+burger.addEventListener('click', showmenu);
+exitNav.addEventListener('click', hidemenu);
